1854e38b6a822a903a33a0d16db0e274ea057e7a
eI1on <77742226+eI1on@users.noreply.github.com>
2024-02-13 18:34:06 +0200

    Added BM_ISPaintAction.lua

---
+++ Contents/mods/BuildingMenu/media/lua/client/TimedActions/BM_ISPaintAction.lua
@@ -0,0 +1,26 @@
+require("BuildingObjects/TimedActions/ISPaintAction")
+
+local oldISPaintActionPerform = ISPaintAction.perform;
+
+local spriteMap = {
+    ["building_menu_01_0"] = "building_menu_01_24",
+    ["building_menu_01_1"] = "building_menu_01_25",
+    ["building_menu_01_8"] = "building_menu_01_24",
+    ["building_menu_01_9"] = "building_menu_01_25"
+};
+
+--- Overrides the perform function of ISPaintAction to change the sprite of the thumpable based on its current sprite.
+--- @param self ISPaintAction
+function ISPaintAction:perform()
+    local currentIsoSprite = self.thumpable:getSprite();
+    local currentSpriteName = currentIsoSprite:getName();
+    local newSpriteName = spriteMap[currentSpriteName];
+
+    if isDebugEnabled() then print("[Building Menu DEBUG] currentSpriteName: ", currentSpriteName, " newSpriteName:", newSpriteName); end
+
+    if newSpriteName then
+        self.thumpable:setSpriteFromName(newSpriteName);
+    end
+
+    oldISPaintActionPerform(self);
+end
